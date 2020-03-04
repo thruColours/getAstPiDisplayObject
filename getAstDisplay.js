b80f952a89c2173d36d60a1238c2eb9b3f9e1eb6
thruColours <49875572+thruColours@users.noreply.github.com>
2020-03-04 17:47:12 +0000

    Add files via upload

--- getAstDisplay.js
+++ getAstDisplay.js
@@ -1,5 +1,5 @@
 
-// const api_url = 'https://api.nasa.gov/neo/rest/v1/feed?start_date=2020-02-10&end_date=2020-02-10&api_key=oc2osS1PDgSZWDOphc4r10HtzpHVZacT59v3drpp';
+// const api_url = 'https://api.nasa.gov/neo/rest/v1/feed?start_date=2020-02-23&end_date=2020-02-23&api_key=oc2osS1PDgSZWDOphc4r10HtzpHVZacT59v3drpp';
 const api_url = 'https://api.nasa.gov/neo/rest/v1/feed/today?detail=true&api_key=oc2osS1PDgSZWDOphc4r10HtzpHVZacT59v3drpp';
 //rate limit once per second
 
@@ -14,18 +14,29 @@ async function getAst() {
   const displayDate = Object.keys(near_earth_objects)[0];
 
   let totalHaz = 0;
+
   let astTimes = [];
+  let hazTimes = [];
+
   let allId = [];
   let hazId = [];
+
   let hazString = hazId.toString();
   let hazTimeDisplay = [];
   let hazTimeBr = [];
-  // let hazTimeDisHours = Date.hazTimeDisplay.getHours();
 
-  // const fakeAst = [{
-  //   unixTime: 1580921024000,
-  //   is_potentially_hazardous_asteroid: true
-  // }];
+    var today = new Date();
+    var hours = today.getHours();
+    if (hours < 10) {
+      hours = "0" + hours;
+    };
+    var minutes = today.getMinutes();
+    if (minutes < 10) {
+      minutes = "0" + minutes;
+    };
+  var time = hours + ":" + minutes;
+
+  // let hazTimeDisHours = Date.hazTimeDisplay.getHours();
 
   //slice just time from each element in array????
   // hazTimeDisplay.forEach(item  =>
@@ -46,6 +57,7 @@ async function getAst() {
     if (item.is_potentially_hazardous_asteroid === true) {
     totalHaz++;
     hazId.push(item.id);
+    hazTimes.push(item.close_approach_data[0].epoch_date_close_approach);
     hazTimeDisplay.push(item.close_approach_data[0].close_approach_date_full);
     // hazTimeDisplay.push(item.close_approach_data[0].close_approach_date_full.replace(",", "<br />"));
     // console.log(item.id);
@@ -58,16 +70,14 @@ async function getAst() {
   // hazTimeDisplay.push("2020-Feb-06 16:20")
   console.log(hazTimeDisplay);
 
-  astTimes.push(1581008220000);
+  // astTimes.push(1581008220000);
   // console.log(astTimes);
+
+  // allId.push("420");
+  // console.log(allId);
   //
-  allId.push("420");
-  console.log(allId);
-
-  hazId.push("420");
-  console.log(hazId);
-
-  allId.push(hazString);
+  // hazId.push("420");
+  // console.log(hazId);
 
   //turn milliseconds into seconds (coeff) and use this to round current time to nearest minute
   //so can check if time of hazardous asteroid falls within the current minute
@@ -75,38 +85,51 @@ async function getAst() {
   let date = new Date();
   let rounded = new Date(Math.round(date.getTime()/ coeff) * coeff);
 
+  // differenceMs = [hazTimes - rounded];
+  // displayDistMins = differenceMs/60000;
+  // console.log(displayDistMins);
+  // console.log(differenceMs);
+  // get current time in Unix
   console.log(date.getTime());
+
+  //create array with time til close approach in minutes (do for both Haz and nonHaz)
+  hazCalc = x => (x - rounded)/60000;
+  hazDistance = hazTimes.map(hazCalc);
+  console.log(hazDistance);
+
+  hazCalc3 = x => x.slice(12, 17);
+  hazSlice = hazTimeDisplay.map(hazCalc3);
+  hazSlice.sort();
+  console.log(hazSlice);
+
+  if (hazTimes == 0) {
+    hazSlice.push("ZERO");
+  };
 
   // near_earth_objects.[2020-01-31].id.push("420");
   // console.log(data.id);
 
   //convert id of hazardous to string, add to allId array and check the existence of string correlates with time
-  if (astTimes.includes(rounded.getTime())&&allId.includes(hazString)) {
-    console.log("animation");
+  if (astTimes.includes(rounded.getTime())&&hazTimes.includes(rounded.getTime())) {
+    console.log("potentially hazardous animation");
+
+  }
+  else if (astTimes.includes(rounded.getTime())){
+    console.log("normal animation");
+
   }
   else {
     console.log("no animation");
   };
 
-  // //works, but better check needed
-  // if (astTimes.includes(rounded.getTime())&&hazId.includes("420")) {
-  //   console.log("animation");
+  // if (displayDistMin <= 30) {
+  //
   // }
-  // else {
-  //   console.log("no animation");
-  // };
-
-  // //fake try 1
-  // if (astTimes.includes(rounded.getTime())&&fakeAst.is_potentially_hazardous_asteroid === true) {
-  //   console.log("animation");
-  // }
-  // else {
-  //   console.log("no animation");
-  // };
 
   document.getElementById('totalAsteroids').textContent = element_count;
   document.getElementById('potentiallyHazardous').textContent = totalHaz;
-  document.getElementById('hazTimeDisplay').textContent = hazTimeDisplay.join('\n');
+  document.getElementById('hazTimeDisplay').textContent = hazSlice.join("\r\n");
+  document.getElementById('time').textContent = time;
   // document.getElementById('date').textContent = displayDate;
   // document.getElementById('vel').textContent = velocity;
 
